74484dcca7e013e816cf74aa579ec5dadc0351e9
Giacomo <giacomobertolazzi7@gmail.com>
2017-11-01 18:20:44 +0100

    REFACTORING  per capire meglio per il futuro

--- src/minefield/test/TestField.java
+++ src/minefield/test/TestField.java
@@ -2,10 +2,13 @@ package minefield.test;
 
 import org.junit.Test;
 
+import minefield.Field;
 import minefield.FieldNotSafe;
 import minefield.FieldSafe;
 
 public class TestField {
+	
+	private Field field;
 	
 	@Test
 	public void test01(){
